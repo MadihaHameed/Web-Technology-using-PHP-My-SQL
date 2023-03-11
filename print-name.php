6d24e1d0c7788efca681ed949fce1f273f7459b1
Madiha Hameed <52617926+MadihaHameed@users.noreply.github.com>
2023-03-11 23:12:16 +0500

    Delete print-name.php

--- print-name.php
+++
@@ -1,6 +0,0 @@
-<?php // This is the opening PHP tag. It indicates the start of a PHP script.
-//This line declares a variable called $name and assigns it a string value of "Your Name".
-$name = "Madiha "; // Replace "Your Name" with your actual name
-//This line uses the echo statement to output the string, "." is used to concatenate strings
-echo "My name is " . $name; // semicolon is compulsary as EOS in PHP
-//This is the closing PHP tag ?>
